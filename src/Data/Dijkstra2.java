5786476d4943fb99f6aa1aeead1d6f0f67183d32
JiEung2 <181757@jnu.ac.kr>
2023-04-27 20:17:30 +0900

    다익스트라에 알고리즘 123 추가하여 만든 알고리즘

--- src/Data/Dijkstra2.java
+++ src/Data/Dijkstra2.java
@@ -1,9 +1,29 @@
 package Data;
 
-import java.util.ArrayList;
 import java.util.*;
+import java.io.*;
+
+class Point{
+	double m, s;
+	public Point(double m, double s) {
+		this.m = m;
+		this.s = s;
+	}
+}
 
 public class Dijkstra2 {
+	static ArrayList<Point> list;
+	static LinkedHashSet<Point> set;
+	static int n;
+	static int s;
+	static int t;
+	static double[][] sd;
+	static double[][] tt;
+	
+	static String fileName = "RSPP_500_5_Dijkstra";
+	static String filePath = "C:\\Users\\``\\eclipse-workspace\\cplex\\result\\";
+	static String fileType = ".txt";
+	
 	//가장 짧은 거리를 가지고 있는 노드를 찾음
 	static int findMinDistance(double d[], boolean T[], int n) {
 		double min = Double.MAX_VALUE;
@@ -18,11 +38,18 @@ public class Dijkstra2 {
 		return min_index;
 	}
 	
-	static void dijkstra(double tt[][], int s, int n, int t) {
+	static double[] dijkstra(double c1, double c2) {
+
 		double d[] = new double[n];
 		boolean T[] = new boolean[n];
 		int p[] = new int[n];
+		double[][] obj = new double[n][n];
 		
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				obj[i][j] = c1*tt[i][j] + c2*sd[i][j];
+			}
+		}
 		Arrays.fill(d, Double.MAX_VALUE);
 		Arrays.fill(T, false);
 		Arrays.fill(p, -1);
@@ -34,13 +61,13 @@ public class Dijkstra2 {
 			T[u] = true;
 			//가장 짧은 거리를 가진 노드를 추가
 			for(int v = 0; v <n; v++) {
-				if(!T[v] && tt[u][v]!=0 && d[u]!=Double.MAX_VALUE && d[u]+tt[u][v] < d[v]) {
-					d[v] = d[u] + tt[u][v];
+				if(!T[v] && obj[u][v]!=0 && d[u]!=Double.MAX_VALUE && d[u]+obj[u][v] < d[v]) {
+					d[v] = d[u] + obj[u][v];
 					p[v] = u;
 				}
 			}
 		}
-		System.out.println(s+" "+t+"의 최단거리는");
+		System.out.println("최단거리는");
 		System.out.println(d[t]);
 		
 		//최단경로 추적
@@ -55,7 +82,7 @@ public class Dijkstra2 {
         // 경로 뒤집기
         Collections.reverse(path);
         
-        double m1 = 0, s1 = 0;
+        //double m1 = 0, s1 = 0;
         
         int[] arr = new int[path.size()];
         
@@ -69,27 +96,105 @@ public class Dijkstra2 {
             }
         }
         
-        /*for(int i=0; i<arr.length-1; i++) {
-        	m1 += GetData.TravelTime[arr[i]][arr[i+1]];
-        	s1 += GetData.StandardDeviation[arr[i]][arr[i+1]];
+        double answer[] = new double[2];
+        for(int i=1; i<arr.length; i++) {
+        	answer[0] += tt[arr[i-1]][arr[i]];
+        	answer[1] += sd[arr[i-1]][arr[i]];
         }
         System.out.println();
-        System.out.println("m1 = "+m1+" s1 = "+ s1);
-        int c1 = 1, c2 = 0;
-        System.out.println(m1*c2+" "+c1*s1);
-        */
+        return answer;
     }
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		
+		long start = System.currentTimeMillis();
+		set = new LinkedHashSet<>();
 		GetData.main(null);
-		int n = GetData.nodenum;
-		int s = GetData.s - 1;
-		int t = GetData.d - 1;
-		double[][] tt = GetData.TravelTime;
+		App.solveMe();
+
+		double m1, m2, s1, s2;
+		n = GetData.nodenum;
+		s = GetData.s-1;
+		t = GetData.d-1;
+		sd = new double[n][n];
+		tt = new double[n][n];
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				sd[i][j] = GetData.StandardDeviation[i][j]*GetData.StandardDeviation[i][j];	
+			}
+		}
+		tt = GetData.TravelTime;
 		
-		Dijkstra2.dijkstra(tt, s, n, t);
+		m1 = Dijkstra2.dijkstra(1, 0)[0];
+		s1 = Dijkstra2.dijkstra(1, 0)[1];
+		set.add(new Point(m1,s1));
+		//System.out.println(Dijkstra2.dijkstra(1, 0)[0]);
+		//System.out.println(m1);
+		m2 = Dijkstra2.dijkstra(0, 1)[0];
+		s2 = Dijkstra2.dijkstra(0, 1)[1];
+		set.add(new Point(m2,s2));
+		System.out.println(m2+" "+ s2);
+		NEWSOL(m1,s1,m2,s2);
+		
+		Iterator<Point> iter = set.iterator();
+		
+		while(iter.hasNext()) {
+			Point e = iter.next();
+			try {
+				BufferedWriter fw = new BufferedWriter(new FileWriter(filePath+fileName+fileType, true));
+				fw.newLine();
+				fw.write(Double.toString(e.m));
+				fw.write(" ");
+				fw.write(Double.toString(e.s));
+				fw.write(" ");
+				fw.write(Double.toString((App.Obj*1.1 - e.m)/Math.sqrt(e.s)));
+				//fw.newLine();
+				
+				
+				fw.flush();
+				fw.close();
+			}
+			catch(Exception a) {
+				a.getStackTrace();
+			}
+			System.out.println(e.m +" "+e.s+" "+ (App.Obj*1.1 - e.m)/Math.sqrt(e.s) );
+		}
+		long end = System.currentTimeMillis();
+		double time = (end - start) / 1000.0;
+		
+		System.out.println(set.size());
+		try {
+			BufferedWriter fw = new BufferedWriter(new FileWriter(filePath+fileName+fileType, true));
+			fw.newLine();
+			fw.write(Integer.toString(set.size()));
+			fw.newLine();
+			fw.write(Double.toString(time));
+			
+			fw.flush();
+			fw.close();
+		}
+		catch(Exception a) {
+			a.getStackTrace();
+		}
+		System.out.println("실행시간 = " + time + "초");
+		System.exit(0);
 	}
+	public static void NEWSOL(double m1, double s1, double m2, double s2) throws IOException {
+		double m3=0, s3=0;
+		if(m1!=m2) {
+			double tmp = -((s2-s1)/(m2-m1));
+			m3 = Dijkstra2.dijkstra(tmp, 1)[0];
+			s3 = Dijkstra2.dijkstra(tmp, 1)[1];
+		}
+		else {
+			m3 = Dijkstra2.dijkstra(1,0)[0];
+			s3 = Dijkstra2.dijkstra(1,0)[1];
+		}
+		if((m3!=m1|| s3!=s1) && (m3!=m2 ||s3!=s2)) {
+			set.add(new Point(m3,s3));
+			NEWSOL(m1,s1,m3,s3);
+			NEWSOL(m2,s2,m3,s3);
+		}
+	}	
 
 }
